f49f0623ef101550ececafe2974d9a4b5483ac7e
Vishm <londhe.vishalps@gmail.com>
2025-12-17 22:51:12 +0100

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'idea-inbox-v1';
+const ASSETS = ['./', './index.html', './index.tsx', './App.tsx'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
+});
