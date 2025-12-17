d5c4de2f8df33dc20991d664bdf5ea3d92b5ab6f
Vishm <londhe.vishalps@gmail.com>
2025-12-18 00:17:33 +0100

    Log app rendering in index.js
    Added a console log to indicate app rendering.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// index.js - Simplified version
 import React from 'https://esm.sh/react@18';
 import ReactDOM from 'https://esm.sh/react-dom@18';
 import htm from 'https://esm.sh/htm@3.1.1';
@@ -10,3 +11,5 @@ ReactDOM.render(
   html`<${App} />`,
   document.getElementById('root')
 );
+
+console.log('App rendered from index.js');
