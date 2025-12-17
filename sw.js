b4be84a729a32dc16469683404b18b5ff5b926f7
Vishm <londhe.vishalps@gmail.com>
2025-12-17 23:28:12 +0100

    Update service worker cache name and add activation event

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'idea-inbox-v3';
+const CACHE_NAME = 'idea-inbox-v5';
 const ASSETS = [
   './',
   './index.html',
@@ -9,9 +9,21 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
 });
