0911a6282b08ed8c5b5b091510be1de41992cca8
Vishm <londhe.vishalps@gmail.com>
2025-12-17 23:17:03 +0100

    Delete types.ts

--- types.ts
+++
@@ -1,3 +0,0 @@
-export interface NoteData { title: string; content: string; tags: string[]; }
-export interface AppSettings { vaultName: string; folderPath: string; }
-export enum ViewState { EDITING = 'EDITING', PREVIEW = 'PREVIEW' }
